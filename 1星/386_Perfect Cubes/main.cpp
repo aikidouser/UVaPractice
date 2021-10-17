89757b9f3839fde099a704a48e8bf7f0c8f555fa
aikidouser <t36085520@icloud.com>
2021-10-17 16:05:17 +0800

    Finish UVa 386

---
+++ 1星/386_Perfect Cubes/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+	// freopen("output.txt", "w", stdout);
+	
+	for(int a=2; a<=200; a++){
+		for(int b=2; b<a; b++){
+			for(int c=b; c<a; c++){
+				for(int d=c; d<a; d++){
+					if(a*a*a == b*b*b + c*c*c + d*d*d){
+						cout << "Cube = " << a << ", ";
+						cout << "Triple = (" << b << "," << c << ","<< d << ")" << endl;
+					}
+				}
+			}
+		}
+	}
+
+	// fclose(stdout);
+
+	return 0;
+}
