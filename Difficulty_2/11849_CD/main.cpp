e757e7e426cc82bf059b4a36d4fc46b79339d98f
aikidouser <t36085520@icloud.com>
2022-04-03 03:57:57 +0800

    Add .gitignore

--- Difficulty_2/11849_CD/main.cpp
+++ Difficulty_2/11849_CD/main.cpp
@@ -3,28 +3,25 @@
 
 using namespace std;
 
-int main(){
+int main() {
+    int N, M;
+    set<int> cd_set;
 
-	int N, M;
-	set<int> cd_set;
+    while (cin >> N >> M, N || M) {
+        int cd, same = 0;
 
-	while(cin >> N >> M, N || M){
-
-		int cd, same = 0;
-
-		for(int i=0; i<N; i++){
-
-			cin >> cd;
-			cd_set.insert(cd);
-		}
-		for(int i=0; i<M; i++){
-
-			cin >> cd;
-			if(cd_set.find(cd) != cd_set.end()){
-				same++;
-			}
-		}
-		cout << same << endl;
-		cd_set.clear();
-	}
+        for (int i = 0; i < N; i++) {
+            cin >> cd;
+            cd_set.insert(cd);
+        }
+        for (int i = 0; i < M; i++) {
+            cin >> cd;
+            if (cd_set.find(cd) != cd_set.end()) {
+                same++;
+            }
+        }
+        cout << same << endl;
+        cd_set.clear();
+    }
+    return 0;
 }
